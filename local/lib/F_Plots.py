180dddb82324d15d606980ec6e42f92042fb1620
Danilo Diaz Valencia <ddiazva13@gmail.com>
2020-12-01 15:50:18 -0500

    Funcion IoU implemnetdad en Numpy

--- local/lib/F_Plots.py
+++ local/lib/F_Plots.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import os
 from PIL import Image
+from . import IoU as iou
 import numpy as np
 import pandas as pd
+import cv2
 
-def display_random_image (images, label,show_box = False,image_path = 'local/data/imgs'):
+def display_random_image (images, label, show_box = True ,image_path = 'local/data/imgs'):
     """
         Display a random image
     """
@@ -42,6 +44,60 @@ def display_random_image (images, label,show_box = False,image_path = 'local/dat
         plt.show()
 
 
+def display_random_image_Pre (Images,Images_Pre, show_box = True,image_path = 'local/data/imgs'):
+    """
+        Display a random image
+    """
+    
+    index_img = np.random.randint(Images.shape[0])
+
+    img = Image.open(os.path.join(image_path,Images.iloc[index_img][0]))
+
+
+    if show_box == True:
+        img_n = np.array(img)
+        len_y , len_x , _ = img_n.shape
+
+        xmin = Images.iloc[index_img][1] * len_x
+        xmax = Images.iloc[index_img][2] * len_x
+        ymin = Images.iloc[index_img][3] * len_y
+        ymax = Images.iloc[index_img][4] * len_y
+
+        xmin_p = Images_Pre[index_img][0] * len_x
+        xmax_p = Images_Pre[index_img][1] * len_x
+        ymin_p = Images_Pre[index_img][2] * len_y
+        ymax_p = Images_Pre[index_img][3] * len_y
+
+
+        plt.figure(figsize=(15,8))
+        plt.imshow(img)
+        plt.title('Image #{} : Formato : {} '.format(index_img,Images.iloc[index_img][6]))
+        plt.plot((xmin,xmax),(ymin,ymin),"r")
+        plt.plot((xmin,xmax),(ymax,ymax),"r")
+        plt.plot((xmin,xmin),(ymax,ymin),"r")
+        plt.plot((xmax,xmax),(ymax,ymin),"r")
+
+
+        Iou_values = iou.bb_intersection_over_union(Images.iloc[index_img].values[1:5],Images_Pre[index_img])
+
+
+        plt.text(len_x, len_y, 'IoU: {:.4f}'.format(Iou_values[0]),
+                verticalalignment='bottom', horizontalalignment='right',
+                color='r', fontsize=15,bbox=dict(fill=True, edgecolor='red', linewidth=2))
+
+                
+
+        plt.plot((xmin_p,xmax_p),(ymin_p,ymin_p),"g")
+        plt.plot((xmin_p,xmax_p),(ymax_p,ymax_p),"g")
+        plt.plot((xmin_p,xmin_p),(ymax_p,ymin_p),"g")
+        plt.plot((xmax_p,xmax_p),(ymax_p,ymin_p),"g")
+        
+        plt.show()
+
+        
+    else:
+        print('Implementar')
+
 #function to show a batch of three images
 
 def imshow_batch_of_three(batch, show_box=True):
@@ -74,3 +130,11 @@ def imshow_batch_of_three(batch, show_box=True):
             axarr[i].plot((int(boxes_batch[i][0]*len_x),int(boxes_batch[i][1]*len_x)),(int(boxes_batch[i][3]*len_y),int(boxes_batch[i][3]*len_y)),"r")
             axarr[i].plot((int(boxes_batch[i][0]*len_x),int(boxes_batch[i][0]*len_x)),(int(boxes_batch[i][2]*len_y),int(boxes_batch[i][3]*len_y)),"r")
             axarr[i].plot((int(boxes_batch[i][1]*len_x),int(boxes_batch[i][1]*len_x)),(int(boxes_batch[i][2]*len_y),int(boxes_batch[i][3]*len_y)),"r")
+
+
+def Display_Examples(Examples):
+    """
+        Display a random image
+    """
+
+ 
